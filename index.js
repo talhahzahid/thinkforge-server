fe43e417e73c90c817fb0715d3136274175442ca
Talha Zahid <talhazahid415@gmail.com>
2025-06-25 14:19:16 +0500

    versal

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectdb from "./src/db/index.js";
 import router from "./src/routes/user.routes.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 const app = express();
-const port = 8000;
+const port = process.env.port;
 app.use(express.json());
-app.use(cookieParser()) 
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -15,8 +15,8 @@ app.use("/api/v1", router);
 
 connectdb()
   .then(() => {
-    app.listen(port, () => {
-      console.log("Server Is Running At Port", port);
+    app.listen(process.env.port, () => {
+      console.log("Server Is Running At Port", process.env.port);
     });
   })
   .catch((err) => {

c7cc2c3d9c081235aa52062c5a45582c61ccd8a4
Talha Zahid <talhazahid415@gmail.com>
2025-07-04 11:27:43 +0500

    correct routes

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -10,7 +10,7 @@ import { authenticate } from "../middleware/userRef.middleware.js";
 const router = express.Router();
 import { upload } from "../middleware/multer.middleware.js";
 // router.post("/addblog", authenticate, upload.single("imageUrl"), addBlog);
-router.post("/addblog", upload.single("imageUrl"))
+router.post("/addblog", upload.single("imageUrl"), addBlog);
 router.get("/getblog", getAllBlog);
 router.get("/getblogbyid/:id", authenticate, getBlogById);
 router.post("/deleteblog/:id", authenticate, deleteBlog);
